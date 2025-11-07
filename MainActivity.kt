e8e0e54fa45045ffcc1de2a4925c447fc8c1c5f0
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2025-11-07 01:32:59 +0100

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -15,7 +15,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val baseUrl = "https://api.openweathermap.org/"
-
+    FirebaseApp.initializeApp(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
